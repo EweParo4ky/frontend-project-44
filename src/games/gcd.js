761dc4c785a588603021e844c35df7e46b3a407a
Ilya Sukharev <imsworkfor@gmail.com>
2022-10-09 10:10:24 +0300

    add getGcd function

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,13 +1,27 @@
-import { getRandomNumber, getGcdTwoNumbers } from '../utilities.js';
+import { getRandomNumber } from '../utilities.js';
 
 import startGameEngine from '../index.js';
 
 const description = 'Find the greatest common divisor of given numbers.';
 
+function getGcd(number1, number2) {
+  if (typeof number1 !== 'number' || typeof number2 !== 'number') {
+    return false;
+  }
+  let a = Math.abs(number1);
+  let b = Math.abs(number2);
+  while (b) {
+    const t = b;
+    b = a % b;
+    a = t;
+  }
+  return a;
+}
+
 function generateRound() {
   const number1 = getRandomNumber();
   const number2 = getRandomNumber();
-  const correctAnswer = String(getGcdTwoNumbers(number1, number2));
+  const correctAnswer = String(getGcd(number1, number2));
   const question = `${number1} ${number2}`;
   return [question, correctAnswer];
 }
