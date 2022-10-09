b066d65c648afeaa39cde045bdafe59889865281
Ilya Sukharev <imsworkfor@gmail.com>
2022-10-09 10:11:23 +0300

    delete getGcd function

--- src/utilities.js
+++ src/utilities.js
@@ -3,22 +3,8 @@ function getRandomNumber() {
   return number1;
 }
 
-function getGcdTwoNumbers(number1, number2) {
-  if (typeof number1 !== 'number' || typeof number2 !== 'number') {
-    return false;
-  }
-  let a = Math.abs(number1);
-  let b = Math.abs(number2);
-  while (b) {
-    const t = b;
-    b = a % b;
-    a = t;
-  }
-  return a;
-}
-
 function getRandomInRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export { getRandomNumber, getGcdTwoNumbers, getRandomInRange };
+export { getRandomNumber, getRandomInRange };
